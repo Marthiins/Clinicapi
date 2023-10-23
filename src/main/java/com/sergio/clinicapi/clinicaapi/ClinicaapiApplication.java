f33fcc3fb9e01582997bc49b429c3dd32d0593dd
Marthiins <sergio.marthiins@gmail.com>
2023-10-23 18:56:21 -0300

    Projeto criado e dependencias

---
+++ src/main/java/com/sergio/clinicapi/clinicaapi/ClinicaapiApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.clinicapi.clinicaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergio/clinicapi/clinicaapi/ClinicaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.clinicapi.clinicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
